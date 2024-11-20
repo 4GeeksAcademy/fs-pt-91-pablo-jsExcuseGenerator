5c77ae2b36746ee5cd14bfa91ff4dccf0d0a82ba
Pablo Enguix Llopis <56578000+PabloEnguix09@users.noreply.github.com>
2024-11-20 11:06:19 +0000

    Project done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateRandomNumber(maxElements) {
+  return Math.floor(Math.random() * maxElements);
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const excuse = document.querySelector("#excuse");
+
+  excuse.innerHTML =
+    who[generateRandomNumber(who.length)] +
+    " " +
+    action[generateRandomNumber(action.length)] +
+    " " +
+    what[generateRandomNumber(what.length)] +
+    " " +
+    when[generateRandomNumber(when.length)];
 };
